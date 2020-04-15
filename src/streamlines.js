942cfbc46a56a02f1b8ed9a471a793be8cec19e4
thetylerwolf <tylernicholaswolf@gmail.com>
2020-04-15 16:31:10 +0200

    incremental refactor

--- src/streamlines.js
+++ src/streamlines.js
@@ -2,35 +2,36 @@ import chroma from 'chroma-js'
 import streamlines from '@anvaka/streamlines'
 import tooloud from 'tooloud'
 import * as d3 from 'd3'
+import computeCurl from './lib/curlNoise'
 
 var canvas = document.getElementById("canvas"),
     context = canvas.getContext("2d"),
-    canvasWidth = window.innerWidth,
-    canvasHeight = window.innerHeight
+    canvasWidth = 500 || window.innerWidth,
+    canvasHeight = 500 || window.innerHeight
 
 let dpr = window.devicePixelRatio || 1,
   width = canvas.width = Math.floor( canvasWidth * dpr ),
   height = canvas.height = Math.floor( canvasHeight * dpr )
 
-tooloud.Perlin.setSeed(Math.floor(Math.random() * 1000))
+const noiseSeed = Math.floor(Math.random() * 1000)
+
+tooloud.Perlin.setSeed(noiseSeed)
+tooloud.Simplex.setSeed(noiseSeed)
+
 const { noise } = tooloud.Fractal,
   perlin = tooloud.Perlin.noise,
   simplex = tooloud.Simplex.noise
-
-// context.lineWidth = 0.1;
-
-
 
 const hSections = Math.ceil( height/5 ),
   wSections = Math.ceil( width/5 )
 const colorScale = chroma.scale(['#5D7190','#F78481'])
   .mode('rgb').colors(hSections)
 
-const points = d3.range(0, wSections * hSections).map((d) => {
+const points = d3.range(0, 1000).map((d) => {
   return {
     x: (d * 10) % width * (15/width),
     y: Math.floor(d / width)*10 * 15/width,
-    // value: 1000 * Math.random()
+    value: 1000 * Math.random()
     // vx: 0,
     // vy: 0,
     // color: colorScale[y]
@@ -39,7 +40,7 @@ const points = d3.range(0, wSections * hSections).map((d) => {
 })
 
 const packingData = { children: points },
-  h = d3.hierarchy(packingData).sum(d => 1),
+  h = d3.hierarchy(packingData).sum(d => d.value),
   pack = d3.pack().size([15, 15]),
   pointData = pack( h ).leaves().map(d => ({ x: Math.round(d.x), y: Math.round(d.y) }))
 
@@ -57,7 +58,7 @@ function render() {
   streamlines({
     // As usual, define your vector field:
     vectorField(p) {
-      let noiseFactor = 0.2,
+      let noiseFactor = 0.06,
         xIn = p.x * noiseFactor,
         yIn = p.y * noiseFactor
 
@@ -72,9 +73,9 @@ function render() {
     boundingBox: { left: 0, top: 0, width: 15, height: 15 },
     seed: pointData,
     // Separation distance between new streamlines.
-    dSep: 0.05,
+    dSep: 0.1,
     // Distance between streamlines when integration should stop.
-    dTest: 0.018,
+    dTest: 0.05,
     // Integration time step (passed to RK4 method.)
     timeStep: 0.01,
 
@@ -91,7 +92,7 @@ function render() {
 
     // },
     onStreamlineAdded(points, config) {
-      context.lineWidth = 2.5
+      context.lineWidth = 2
       // context.strokeStyle = `rgba(128, 128, 175, 0.6)`;
       context.strokeStyle = colorScale[Math.round(points[0].y * 6)]
       context.lineJoin = "round";
@@ -126,25 +127,4 @@ function transform(pt, boundingBox) {
     x: tx * width,
     y: (1 - ty) * height
   }
-}
-
-function computeCurl(x, y, z){
-  var eps = 0.0001;
-
-  //Find rate of change in X direction
-  var n1 = perlin(x + eps, y, z);
-  var n2 = perlin(x - eps, y, z); 
-
-  //Average to find approximate derivative
-  var a = (n1 - n2)/(2 * eps);
-
-  //Find rate of change in Y direction
-  var n1 = perlin(x, y + eps, z); 
-  var n2 = perlin(x, y - eps, z); 
-
-  //Average to find approximate derivative
-  var b = (n1 - n2)/(2 * eps);
-
-  //Curl
-  return [b, -a];
 }
