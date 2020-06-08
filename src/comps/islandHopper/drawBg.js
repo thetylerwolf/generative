a19f29e0633ffc838fc46297b6c7b58482eadfee
Tyler Wolf <tylernicholaswolf@gmail.com>
2020-06-08 17:13:00 +0200

    bg

--- src/comps/islandHopper/drawBg.js
+++ src/comps/islandHopper/drawBg.js
@@ -1,91 +1,96 @@
 const chroma = require('chroma-js')
-const { poissonSampler } = require("../../technique")
+const { poissonSampler, ColorSampler } = require("../../technique")
 const { WaterColor } = require("../../brush")
 const { gaussianRand } = require('../../utils')
 
 module.exports = function drawBg(context, width, height, colors, bgColor) {
+
   // const bgColor = chroma.mix('#fff', randomColors[0], 0.1)
   // const bgColor = '#ffe6cc' // orange
   // const bgColor = '#ccdbff' // blue
   // const bgColor = '#141d2f' // darker blue
   // const bgColor = '#26375a' // dark blue
   // const bgColor = '#010a18' // dark blue
+  context.fillStyle = '#fff'
+  context.rect(0, 0, width, height)
+  context.fill()
   context.fillStyle = bgColor
   context.rect(0, 0, width, height)
   context.fill()
 // return
-  let pointData = poissonSampler(width, height, Math.sqrt(width*width + height*height)/2)
+  let colorSampler = new ColorSampler({
+    width,
+    height,
+    // colors: [colors[2], colors[5], colors[2], colors[5]],
+    colors: [...colors, ...colors, ...colors, ...colors],
+    density: 20,
+    // maxCenterRange: 500,
+    type: 'points',
+    // gradientAngle: Math.PI,
+  })
 
-  // let pointData = []
-  // for(let h = 100; h<canvas.height;h+=450) {
-  //   for(let w = 100; w<canvas.width;w+=450) {
-  //     pointData.push({
-  //       x: w,
-  //       y: h
-  //     })
-  //   }
-  // }
-
-  context.globalAlpha = 0.015
-console.log('a')
-  let wcs = pointData.map((point) => {
-
-    // let c = chroma.mix('#fff', randomColors[0],1)
-    let c = [0, colors[2], colors[5]][Math.floor(Math.random() * 3)] || colors[0]
-    if(!c) return
-    c = chroma(c)
-    // c = c.hsl()
-    // c[1] += -0.05 + Math.random() * 0.1
-    // c[2] += -0.05 + gaussianRand() * 0.1
-    // c[3] += -0.6 * (centerDist/dMax) + gaussianRand() * 0.4
-    // c[3] = gaussianRand()
-    // c[3] = 0.2
-    // c[2] += gaussianRand(-0.05, 0.1)
-
-    // c = chroma.hsl(...c)
-    c = c.darken(gaussianRand(0.5,0.5))
-    c = c.desaturate(gaussianRand(0,0.2))
-    // c = c.desaturate()
-
-    c = c.css()
-    // context.fillStyle = c
-
-    // context.globalAlpha = 0.01
-    let wc = new WaterColor(context, {
-      baseRadius: 40,
-      centerX: point.x,
-      centerY: point.y,
+  const maxRadius = 250,
+    pointRadius = Math.min(maxRadius, Math.sqrt(width*width + height*height) / 7),
+    rVariance = 0.3 * pointRadius,
+    wcSettings = {
+      baseRadius: 0.2 * pointRadius,
       numPoints: 6,
       subdivisions: 7,
-      rVariance: 60,
-      initialrVariance: 250,
-      numLayers: 60,
-      noiseFunction: () => gaussianRand(0.3,0.2),
+      initialrVariance: rVariance,
+      numLayers: 30,
+      noiseFunction: () => Math.random(),
+    }
+
+  let pointData = poissonSampler(
+    width,
+    height,
+    pointRadius
+  )
+
+  context.globalAlpha = 0.015
+
+  let wcs = pointData.map((point) => {
+
+    let c = colorSampler.getNearestColor(point.x, point.y, 1, 0)
+    if(!c) return
+
+    c = chroma(c)
+
+    // c = c.brighten(gaussianRand(0.5,0.2))
+    c = c.darken(gaussianRand(0.5,0.2))
+    c = c.desaturate(gaussianRand(0,0.2))
+
+    c = c.css()
+
+    let wc = new WaterColor(context, {
+      ...wcSettings,
+      centerX: point.x,
+      centerY: point.y,
       color: c,
+      rVariance: gaussianRand(0.3, 0.1) * pointRadius,
     })
 
     wc.run()
-    // console.log(wc)
+
     return wc
   }).filter(d => d)
-  console.log('b')
+
   wcs[0].distortedPolygons.forEach((p,i) => {
-    // console.log(i)
     wcs.forEach(wc => {
       context.fillStyle = wc.color
       drawPolygon(wc.distortedPolygons[i])
     })
   })
-  console.log('c')
 
-
+  // drawCircles(context, width, height, 400, 2)
+  // drawCircles(context, width, height, 250, 5)
+  // drawCircles(context, width, height, 250, 5)
 
   function drawPolygon(points) {
     // console.log(points.length)
-    // let polygon = new Path2D()
+
     context.beginPath()
     points.forEach((point,i) => {
-
       let from = point,
         to = points[i+1]
 
@@ -102,4 +107,14 @@ console.log('a')
   }
 
 
+}
+
+function drawCircles(context, width, height, density, radius) {
+  let points = poissonSampler(
+    width,
+    height,
+    Math.sqrt(width*width + height*height) / density
+  )
+
+  points.forEach(p => circle(context, gaussianRand(radius, radius*3/4), p.x, p.y, '#fff'))
 }
